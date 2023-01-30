6e7ed3a16c24ee341293bff1bc916aa101f0b66e
Suhayb Jirde <dribblekayga@gmail.com>
2023-01-30 11:40:07 -0800

    fix some accessibility issue and add a new feature ot mark as read link

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 let unread = document.querySelectorAll('.not-read');
+let markasread = document.querySelector('.mark-read');
+markasread.addEventListener('click', (e)=> {
+    e.preventDefault();
+    unread.forEach(each => {
+        makeItUnRead(each)
+    })
+})
 unread.forEach(each => {
     each.addEventListener('click', (e)=> {
         e.preventDefault();
-        each.classList.remove('not-read');
-        let nfContent = each.querySelector('.nf-content')
-        let dot = each.querySelector('.nf-content .dot')
-        if (dot) nfContent.removeChild(dot)
+        makeItUnRead(each)
     })
 })
+
+function makeItUnRead(notification) {
+    notification.classList.remove('not-read');
+    let nfContent = notification.querySelector('.nf-content')
+    let dot = notification.querySelector('.nf-content .dot')
+    if (dot) nfContent.removeChild(dot)
+}
